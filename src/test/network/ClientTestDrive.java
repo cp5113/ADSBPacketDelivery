4189950cc48d12c8aa96c17d90f91c59f8e067dd
cp511 <cp511@SeokjaeYun>
2019-02-18 16:12:18 +0900

    Test 1

--- src/test/network/ClientTestDrive.java
+++ src/test/network/ClientTestDrive.java
@@ -1,9 +1,58 @@
 package test.network;
 
-public class ClientTestDrive {
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.net.Socket;
 
+public class ClientTestDrive {
+	private final String SERVER_IP   = "25.12.56.47";
+	private final int	 SERVER_PORT = 5153;
+	private final String ID			 = "SJ";
+	
+	Socket fClinetSocket             = null;
+	BufferedReader fBr				 = null;
+	PrintWriter    fPw				 = null;
+	
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Test");
+		ClientTestDrive myMain = new ClientTestDrive();
+		myMain.doStart();
+		
+	}
+	
+	private void doStart() {
+		connetToServer();
+	}
+	
+	private void connetToServer() {
+		// Create Socket
+		try {
+			
+			// Connect
+			fClinetSocket = new Socket(SERVER_IP, SERVER_PORT);
+			
+			// Stream
+			fPw = new PrintWriter(new OutputStreamWriter(fClinetSocket.getOutputStream()));
+			fBr = new BufferedReader(new InputStreamReader(fClinetSocket.getInputStream()));
+			BufferedReader keyboard = new BufferedReader(new InputStreamReader(System.in));
+			
+			// Say Hello
+			fPw.println(ID);
+			fPw.flush();
+			
+			String typing = null;
+			while((typing = keyboard.readLine()) !=null ) {
+				fPw.println(typing);
+				fPw.flush();
+			}
+			
+
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 	}
 }
--- src/test/network/ServerTestDrive.java
+++ src/test/network/ServerTestDrive.java
@@ -1,10 +1,121 @@
 package test.network;
 
-public class ServerTestDrive {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.HashMap;
 
+public class ServerTestDrive {
+	
+	private  final int SERVER_PORT = 5153;
+	private HashMap<String, Object> fClientList = new HashMap<String, Object>();
+	
+	
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+	ServerTestDrive	myMain = new ServerTestDrive();
+	myMain.doStart();
+		
+		
 
 	}
+	
+	private void doStart() {
+		
+		createSocket();
+		
+	}
+	
+	public void createSocket() {
+		
+		try {
+			
+			//Create Server Socket			
+			ServerSocket server = new ServerSocket(SERVER_PORT);
+			
+			// Waiting Signal
+			while(true) {
+				Socket l_socket = server.accept(); // waiting
+				// create Thread and Run
+				ConnectionThread l_newClient = new ConnectionThread(l_socket, fClientList);
+				l_newClient.start();
+				
+			}
+			
+			
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+		
+	}
 
+	
+	
+	private class ConnectionThread extends Thread{
+		Socket         fSocket;
+		BufferedReader fBr     		= null;
+		String         fClientInfo 	= null;
+		
+		public ConnectionThread(Socket a_socket, HashMap<String,Object> a_clientList) {
+			// Set Object
+			fSocket = a_socket;						
+			
+			// Initial connection
+			try {
+				PrintWriter l_pw 	= new PrintWriter(new OutputStreamWriter(a_socket.getOutputStream()));				
+				fBr 				= new BufferedReader(new InputStreamReader(a_socket.getInputStream()));
+				fClientInfo			= fBr.readLine();
+				a_clientList.put(fClientInfo,l_pw);
+				
+				System.out.println("=======================");
+				System.out.println(fClientInfo + " Connected");
+				
+			}catch(Exception e) {
+				
+			}
+			
+		}
+		
+		public void run() {
+			String l_aline = null;
+			
+			try {
+				
+				while((l_aline = fBr.readLine()) != null) {
+					System.out.println("Data : " + l_aline);
+				}
+				
+				
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
